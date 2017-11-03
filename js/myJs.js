494c2e6971abe1856c639e0c4476a3e5c70e8b87
silverglint <33155874+silverGlint@users.noreply.github.com>
2017-11-03 21:59:41 +0800

    preload

--- js/myJs.js
+++ js/myJs.js
@@ -125,30 +125,6 @@ function random(min, max) {
 //ss
 
 
-//js类名操作
-function hasClass(ele, cls) {
-    return ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
-}
-//为指定的dom元素添加样式
-function addClass(ele, cls) {
-    if (!this.hasClass(ele, cls)) ele.className += " " + cls;
-
-}
-//删除指定dom元素的样式
-function removeClass(ele, cls) {
-    if (hasClass(ele, cls)) {
-        var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-        ele.className = ele.className.replace(reg, " ");
-    }
-}
-//如果存在(不存在)，就删除(添加)一个样式
-function toggleClass(ele,cls){
-    if(hasClass(ele,cls)){
-        removeClass(ele, cls);
-    }else{
-        addClass(ele, cls);
-    }
-}
 
 //jQ文档加载时加载
 $(function () {
